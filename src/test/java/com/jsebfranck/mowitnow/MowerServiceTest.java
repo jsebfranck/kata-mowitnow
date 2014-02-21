011487259d0ddf6283456d6301cfad0050ba5d6c
jsebfranck <jsebfranck@gmail.com>
2014-02-21 21:30:33 +0100

    Use parameterized tests as all tests have the same structure

--- src/test/java/com/jsebfranck/mowitnow/MowerServiceTest.java
+++ src/test/java/com/jsebfranck/mowitnow/MowerServiceTest.java
@@ -2,9 +2,13 @@ package com.jsebfranck.mowitnow;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,6 +16,7 @@ import static org.junit.Assert.assertEquals;
  * @see com.jsebfranck.mowitnow.MowerService
  * @author jsebfranck
  */
+@RunWith(Parameterized.class)
 public class MowerServiceTest {
 
     private MowerService mowerService;
@@ -21,56 +26,50 @@ public class MowerServiceTest {
         mowerService = new MowerService();
     }
 
-    @Test
-    public void moveMower_withoutControl_shouldNotMove() {
-        // Given
-        Ground ground = new Ground(10, 10);
-        Mower mower = new Mower(0, 0, Orientation.NORTH);
+    private final Ground ground;
+    private final Mower mower;
+    private final List<MowerControl> mowerControls;
+    private final int expectedX;
+    private final int expectedY;
+    private final Orientation expectedOrientation;
 
-        // When
-        mowerService.moveMower(ground, mower, Collections.<MowerControl>emptyList());
+    @Parameterized.Parameters
+    public static Collection parameters() {
+        return Arrays.asList(new Object[][] {
+                // without controls, should not move
+                { new Ground(10, 10), new Mower(0, 0, Orientation.NORTH), 0, 0, Orientation.NORTH, ctrl()},
 
-        // Then
-        assertThatMowerPositionIs(mower, 0, 0, Orientation.NORTH);
+                // with left control, should turn to left
+                { new Ground(10, 10), new Mower(0, 0, Orientation.NORTH), 0, 0, Orientation.WEST, ctrl(MowerControl.TURN_LEFT)},
+
+                // with right control, should turn to right
+                { new Ground(10, 10), new Mower(0, 0, Orientation.NORTH), 0, 0, Orientation.EAST, ctrl(MowerControl.TURN_RIGHT)},
+
+                // with advance control, should advance
+                { new Ground(10, 10), new Mower(0, 0, Orientation.NORTH), 0, 1, Orientation.NORTH, ctrl(MowerControl.ADVANCE)},
+        });
+    }
+
+    public static List<MowerControl> ctrl(MowerControl... mowerControls) {
+        return (mowerControls == null) ? Collections.<MowerControl>emptyList() : Arrays.asList(mowerControls);
+    }
+
+    public MowerServiceTest(Ground ground, Mower mower, int expectedX, int expectedY, Orientation expectedOrientation, List<MowerControl> mowerControls) {
+        this.ground = ground;
+        this.mower = mower;
+        this.expectedX = expectedX;
+        this.expectedY = expectedY;
+        this.expectedOrientation = expectedOrientation;
+        this.mowerControls = mowerControls;
     }
 
     @Test
-    public void moveMower_withLeftControl_shouldTurnToLeft() {
-        // Given
-        Ground ground = new Ground(10, 10);
-        Mower mower = new Mower(0, 0, Orientation.NORTH);
-
+    public void moveMower() {
         // When
-        mowerService.moveMower(ground, mower, Arrays.asList(MowerControl.TURN_LEFT));
+        mowerService.moveMower(ground, mower, mowerControls);
 
         // Then
-        assertThatMowerPositionIs(mower, 0, 0, Orientation.WEST);
-    }
-
-    @Test
-    public void moveMower_withLeftControl_shouldTurnToRight() {
-        // Given
-        Ground ground = new Ground(10, 10);
-        Mower mower = new Mower(0, 0, Orientation.NORTH);
-
-        // When
-        mowerService.moveMower(ground, mower, Arrays.asList(MowerControl.TURN_RIGHT));
-
-        // Then
-        assertThatMowerPositionIs(mower, 0, 0, Orientation.EAST);
-    }
-
-    @Test
-    public void moveMower_withAdvanceControl_shouldAdvance() {
-        // Given
-        Ground ground = new Ground(10, 10);
-        Mower mower = new Mower(0, 0, Orientation.NORTH);
-
-        // When
-        mowerService.moveMower(ground, mower, Arrays.asList(MowerControl.ADVANCE));
-
-        // Then
-        assertThatMowerPositionIs(mower, 0, 1, Orientation.NORTH);
+        assertThatMowerPositionIs(mower, expectedX, expectedY, expectedOrientation);
     }
 
     private void assertThatMowerPositionIs(Mower mower, int expectedX, int expectedY, Orientation expectedOrientation) {
